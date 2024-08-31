7a4414e443f65c8396bb14ba9ebf188b018dae87
KingSimpa69 <114375407+KingSimpa69@users.noreply.github.com>
2024-08-31 12:59:34 -0600

    Update MasterLoadingScreen.jsx

--- components/MasterLoadingScreen.jsx
+++ components/MasterLoadingScreen.jsx
@@ -4,7 +4,7 @@ const MasterLoadingScreen = () => {
 
     return(
         <div className="loading">
-            <Image alt={"rikyRunning"} width={200} height={200} src={"/images/rikyRunning.gif"} />
+            <Image alt={"rikyRunning"} width={200} height={200} src={"/images/rikyrunning.gif"} />
             <div className="loader"></div>
         </div>
     )
